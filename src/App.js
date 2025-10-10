2ab431a54a10695b0f4dfc3bd47a52911f475e3f
Khushi Garg <khushigarg005@gmail.com>
2025-10-11 02:16:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className= "basic">
+          <input type = "text" placeholder={'+200 nwe samsung tv'}/>
+          <input type = "datetime-local"/>
+        </div>
+        <div className="description">
+          <input type = "text" placeholder={'description'}/>
+        </div>
+        <button type = "submit">Add new transaction</button>
+      </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </main>
   );
 }
 
